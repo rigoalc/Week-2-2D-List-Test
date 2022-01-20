bf5a7de097c6b08015d644c20e053016bc715838
Rigo Alcover <rodigomartinalcover@gmail.com>
2022-01-20 11:27:48 -0500

    Update create_board.py

--- create_board.py
+++ create_board.py
@@ -8,3 +8,11 @@ EXPECTED = [
     ['X', 'X', 'O'],
 ]
 
+def test_create_board():
+    board = [
+        ['*', '*', '*'],
+        ['*', '*', '*'],
+        ['*', '*', '*'],
+    ]
+    board = create_board(board)
+    assert board == EXPECTED
